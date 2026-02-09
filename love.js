1ac5486a18695af4df58fe10e1e3d42d2cf12666
BlackShaggy <kamilszl2003@gmail.com>
2026-02-09 16:19:24 +0100

    love.js

--- love.js
+++ love.js
@@ -173,7 +173,7 @@
         },
         clear: function() {
             var ctx = this.tree.ctx, cirle = this.cirle;
-            var point = cirle.point, scale = cirle.scale, radius = 26;
+            var point = cirle.point, scale = cirle.scale, radius = 35;
             var w = h = (radius * scale);
             ctx.clearRect(point.x - w, point.y - h, 4 * w, 4 * h);
         },
@@ -1064,4 +1064,5 @@
 
 
 })(window);
+
 
